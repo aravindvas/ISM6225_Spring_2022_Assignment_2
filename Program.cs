ffbd04636a10ca06e8b3f3e48b4fbf5f285cca2c
aravindvas <aravindvas2468@gmail.com>
2022-02-11 20:33:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM6225_Spring_2022_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
